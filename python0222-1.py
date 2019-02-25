7f065b61293e2848f646416f81506ce0c9b203a8
hailey0227 <seonyeonhwang@gmail.com>
2019-02-25 15:23:13 +0900

    문자열 사용하기 & 관련 함수 사용하기

--- python0222-1.py
+++ python0222-1.py
@@ -146,3 +146,122 @@ a = "Python is best choice"
 print("문자 'b'가 처음 나온 위치는 : %s" % a.find('b'))
 
 
+# 2019-02-25 수업내용
+
+a = ","
+print(a.join("abcd"))
+
+a = "hi" # 데이터베이스와 연동하게 될 때 사용
+print("영문 대문자로 출력 : %s " % a.upper())
+
+a = "HI"
+print("영문 소문자로 출력 : %s " % a.lower())
+
+
+a = "     hi"
+print("원본 문장 : %s" % a)
+print("왼쪽 공백 지우기 : %s" % a.lstrip())
+
+a = "hi     " # 데이터베이스와 함께 사용
+print("원본 문장 : %s" % a)
+print("오른쪽 공백 지우기 : %s" % a.rstrip())
+
+a = "     hi     " #훨씬 더 많이 사용
+print("원본 문장 : %s" % a)
+print("양쪽 공백 지우기 : %s" % a.strip())
+
+a = "Life is so short"
+print("원본 문장 : %s" % a)
+print("수정된 문장 : %s" % a.replace("Life", "your leg"))
+
+# split() 를 사용하면 지정한 문자로 문자열을 나누어서 문자열 배열에 출력함 / 기상청 RSS 등에 이용
+a = "Life is too short"
+print("원본 문장 : %s" % a)
+print("나누어진 문장 : %s" % a.split())
+
+a = "a:b:c:d"
+print("원본 문장 : %s" % a)
+print("나누어진 문장 : %s" % a.split(":"))
+
+str = "부산, 울산, 경상남도는 2월 28일 오전과 3월 2일 오후~3일 오전에는 기압골 영향으로 비가 오겠으며, 그 밖의 날은 고기압의 영향으로 대체로 맑은 날이 많겠습니다.<br />기온은 평년(최저기온: -4~3℃, 최고기온: 9~12℃)보다 조금 높겠습니다.<br />강수량은 평년(1~3mm)보다 많겠습니다. <br />바다의 물결은 남해동부와 동해남부 전해상에서 3월 2~4일에 기압골 영향으로 1.0~4.0m로 매우 높게 일겠고, 그 밖의 날은 0.5~2.5m로 일겠습니다."
+print(str.split("<br />"))
+
+
+print()
+print("=" * 80)
+print()
+
+test1 = 10.0
+print("화면에 출력한 코드 %f" % test1)
+print("화면에 출력한 코드 %d" % test1)
+
+test1 = 100
+print("화면에 출력한 코드 %d" % test1)
+print("화면에 출력한 코드 %f" % test1)
+
+print()
+print("=" * 80)
+print()
+
+# 문자열 코드를 사용하지 않고 format() 함수를 이용하여 문자열을 형식에 맞게 출력
+# 문자열 코드 대신 {0} 을 사용하여 각종 데이터가 들어갈 부분을 표현
+# 입력될 데이터가 여러개일 경우 {0} {1} {2} {n} 으로 표현
+# 값은 format(첫번째값, 두번째값, 세번째값, n번째값)으로 데이터를 입력
+print(".format() 함수를 이용하여 문자열을 쉽게 표현하기")
+
+# 숫자 대입하기
+print("I eat %d apples" % 3)
+print("I eat {0} apples".format(3))
+
+# 문자열 대입하기
+print("I eat %s apples" % "five")
+print("I eat {0} apples".format("five"))
+
+# 변수를 대입하기
+number = 3
+print("I eat %d apples" % number)
+print("I eat {0} apples".format(number))
+
+# 정수와 실수 대입하기
+number1 = 100
+number2 = 100.0
+print("정수 출력 : %d. \n소수 출력 : %f" % (number1, number2))
+print("정수 출력 : %f. \n소수 출력 : %d" % (number1, number2))
+print("정수 출력 : {0}. \n소수 출력 : {1}".format(number1, number2))
+
+# 2개 이상의 값 넣기
+number = 10
+day = "three"
+print("I eat {0} apples. so I was sick for {1} days.".format(number, day))
+
+# format() 함수 사용시 {index} 대신 {변수이름} 으로 사용이 가능함
+print("I ate {number} apples. so I was sick for {day} days.".format(number = 10, day = 3))
+
+# {index} 와 {변수이름} 을 동시에 혼용해서 사용이 가능함
+print("I ate {0} apples. so I was sick for {day} days.".format(10, day = 3))
+
+# 문자열 왼쪽 정렬
+print("문자열 왼쪽 정렬 : %-10s jane" % "hi" )
+print("문자열 왼쪽 정렬 : {0: <10} jane".format("hi"))
+
+# 문자열 오른쪽 정렬
+print("문자열 오른쪽 정렬 : %10s jane" % "hi" )
+print("문자열 오른쪽 정렬 : {0: >10} jane".format("hi"))
+
+# 문자열 중앙 정렬
+print("문자열 중앙 정렬 : {0: ^10} jane".format("hi"))
+
+# 공백 채우기
+print("문자열 공백 채우기 : {0:!<10} jane".format("hi"))
+print("문자열 형식 채워서 표현하기 : {0:0>10}".format(100))
+
+# 소수점 표현하기
+y = 3.42134234
+# 소수점 4자리까지 표현
+print("{0:0.4f}".format(y)) # . 도 갯수로 취급
+
+# 소수점 5자리까지 표현하고, 전체 자리수는 10자리로 표현
+print("{0:10.5f}".format(y))
+
+# 문자 {} 표현하기 (%를 화면에 출력하듯이 2번 입력)
+print("{{ and }}".format())
